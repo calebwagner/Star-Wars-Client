e6b1e377bbfd2b2ce704c8fde6dbbf19b62e998d
Caleb <calebwagner5@gmail.com>
2022-12-02 14:30:49 -0600

    updated people search

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import { StarWarsProvider } from './characters/StarWarsProvider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <StarWarsProvider>
         <App />
-      </StarWarsProvider>
     </Router>
   </React.StrictMode>
 );
